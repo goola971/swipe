6a2b6c38118d3619ace06ffeca27dfae8a76ca2b
goola971 <janviercharly97114@gmail.com>
2025-12-11 12:44:41 +0100

    save

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,27 @@ import Footer from "./components/footer/footer.tsx";
 import Index from "./pages/index/index.tsx";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route
-                    element={
-                        <>
-                            <Header />
-                            <Outlet />
-                            <Footer />
-                        </>
-                    }
-                >
-                    <Route path="/" element={<Index />} />
-                    <Route path="/ressources" element={<h1>Ressources</h1>} />
-                    <Route path="/about" element={<h1>A propos</h1>} />
-                    <Route path="/plan" element={<h1>Plan</h1>} />
-                    <Route path="/contact" element={<h1>Contact</h1>} />
-                </Route>
-            </Routes>
-        </>
-    );
+	return (
+		<>
+			<Routes>
+				<Route
+					element={
+						<>
+							<Header />
+							<Outlet />
+							<Footer />
+						</>
+					}
+				>
+					<Route path="/" element={<Index />} />
+					<Route path="/ressources" element={<h1>Ressources</h1>} />
+					<Route path="/about" element={<h1>A propos</h1>} />
+					<Route path="/plan" element={<h1>Plan</h1>} />
+					<Route path="/contact" element={<h1>Contact</h1>} />
+				</Route>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,55 +2,68 @@ import "./index.scss";
 import { type JSX } from "react";
 
 function Index(): JSX.Element {
-    return (
-        <>
-            <section className="hero">
-                <h2>Apprenez les compétences qui comptent vraiment.</h2>
-                <p>
-                    Des formations professionnelles, des parcours structurés et
-                    une plateforme conçue pour progresser rapidement.
-                </p>
-                <div className="Buttons">
-                    <button>
-                        Commencer maintenant{" "}
-                        <img
-                            src="icon/arrowTopLeft.svg"
-                            alt="arrow top left icon"
-                        />
-                    </button>
-                    <button>Voir les formations</button>
-                </div>
-                <div className="">
-                    <img
-                        src="/img/home/hero.webp"
-                        alt="background"
-                        className="background"
-                    />
-                    <div>
-                        <button>Opportunité d'emploi</button>
-                        <h3>Ouvrez la porte aux opportunités du numérique.</h3>
-                        <p>
-                            Des parcours courts et structurés pour accéder
-                            rapidement aux métiers du numérique. Réseaux,
-                            systèmes, développement, cybersécurité, bureautique
-                            et design : des compétences concrètes, adaptées aux
-                            besoins du marché et orientées opportunités
-                            professionnelles.
-                        </p>
-                    </div>
-                    <div>
-                        <button>No-code (Webflow, Bubble)</button>
-                        <button>Réseaux & Télécoms</button>
-                        <button>Supervision réseau</button>
-                        <button>VLAN</button>
-                        <button>SEO / Référencement</button>
-                        <button>Pentest débutant</button>
-                        <button>UI Design</button>
-                        <button>Routage (OSPF, BGP)</button>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+	const lstButton = [
+		"No-code (Webflow, Bubble)",
+		"Réseaux & Telecoms",
+		"Supervision réseau",
+		"VLAN",
+		"SEO / Référencement",
+		"Pentest débutant",
+		"UI Design",
+		"Routage (OSPF, BGP)",
+	];
+	return (
+		<>
+			<section className="hero">
+				<h2>Apprenez les compétences qui comptent vraiment.</h2>
+				<p className="description">
+					Des formations professionnelles, des parcours structurés et
+					une plateforme conçue pour progresser rapidement.
+				</p>
+				<div className="Buttons">
+					<button>
+						Commencer maintenant{" "}
+						<img
+							src="icon/arrowTopLeft.svg"
+							alt="arrow top left icon"
+						/>
+					</button>
+					<button>Voir les formations</button>
+				</div>
+				<div className="heroImgContainer">
+					<img
+						src="/img/home/hero.webp"
+						alt="background"
+						className="background"
+					/>
+					<div className="opportunities">
+						<button>Opportunité d'emploi</button>
+						<h3>Ouvrez la porte aux opportunités du numérique.</h3>
+						<p>
+							Des parcours courts et structurés pour accéder
+							rapidement aux métiers du numérique. Réseaux,
+							systèmes, développement, cybersécurité, bureautique
+							et design : des compétences concrètes, adaptées aux
+							besoins du marché et orientées opportunités
+							professionnelles.
+						</p>
+					</div>
+					<div className="buttonsContainer">
+						{/* <button>No-code (Webflow, Bubble)</button>
+						<button>Réseaux & Télécoms</button>
+						<button>Supervision réseau</button>
+						<button>VLAN</button>
+						<button>SEO / Référencement</button>
+						<button>Pentest débutant</button>
+						<button>UI Design</button>
+						<button>Routage (OSPF, BGP)</button> */}
+						{lstButton.map((item, index) => (
+							<button key={index}>{item}</button>
+						))}
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
 export default Index;
