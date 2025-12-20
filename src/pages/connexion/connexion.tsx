127bad8c41cad52fecf33f9e82c30d63f1f30497
goola971 <janviercharly97114@gmail.com>
2025-12-20 18:27:29 +0100

    save

---
+++ src/pages/connexion/connexion.tsx
@@ -0,0 +1,12 @@
+import "./connexion.scss";
+import { type JSX } from "react";
+
+function Connexion(): JSX.Element {
+    return (
+        <div className="connexion">
+            <h1>Connexion</h1>
+        </div>
+    );
+}
+
+export default Connexion;
---
+++ src/pages/inscription/inscription.tsx
@@ -0,0 +1,8 @@
+import "./inscription.scss";
+import { type JSX } from "react";
+
+function Inscription(): JSX.Element {
+    return <></>;
+}
+
+export default Inscription;
