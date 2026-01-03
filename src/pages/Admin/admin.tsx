fb41a3ee8d46e995bcb41b7196f0df9ebffec431
KEDDAA <130056507+Keddaaa@users.noreply.github.com>
2026-01-03 19:02:19 +0100

    8

--- src/pages/Admin/admin.tsx
+++ src/pages/Admin/admin.tsx
@@ -1,32 +1,54 @@
 import "./admin.scss";
 import { type JSX } from "react";
-import { Outlet } from "react-router-dom";
-
+// import { Link } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
+import Overview from "./overview/overview";
 export default function Admin(): JSX.Element {
 	return (
 		<section className="admin">
 			<aside>
 				<div className="titre">
 					<div className="box">
-						<h1>Swipe.</h1>
+						<h1> Swipe.</h1>
 						<p>Administrator</p>
 					</div>
+
 					<img src="icon/admin/switch.svg" alt="" />
 				</div>
 
 				<ul className="menu">
-					<li>Vue d'ensemble</li>
-					<li>Utilisateurs</li>
-					<li>Statistique</li>
-					<li>Notification</li>
-					<li>Paramètres</li>
-					<li>Formation</li>
-					<li>Réservations</li>
+					<li>
+						<img src="icon/admin/overview.svg" alt="" />
+						Vue d'ensemble
+					</li>
+					<li>
+						<img src="icon/admin/user.svg" alt="" />
+						Utilisateurs
+					</li>
+					<li>
+						<img src="icon/admin/stats.svg" alt="" />
+						Statistique
+					</li>
+					<li>
+						<img src="icon/admin/mail.svg" alt="" />
+						Notification
+					</li>
+					<li>
+						<img src="icon/admin/roue.svg" alt="" />
+						Paramètres
+					</li>
+					<li>
+						<img src="icon/admin/formation.svg" alt="" />
+						Formation
+					</li>
+					<li>
+						<img src="icon/admin/reservation.svg" alt="" />
+						Réservations
+					</li>
 				</ul>
 			</aside>
-
 			<div className="content">
-				<Outlet />
+				<Overview></Overview>
 			</div>
 		</section>
 	);
