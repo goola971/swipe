7eb8714bd64238c123f84965d291bd6311d56ad0
KEDDAA <130056507+Keddaaa@users.noreply.github.com>
2026-01-03 18:54:48 +0100

    zz

--- src/pages/Admin/Overview/overview.tsx
+++ src/pages/Admin/Overview/overview.tsx
@@ -1,75 +1,52 @@
-import { Outlet } from "react-router-dom";
 import "./overview.scss";
 import { type JSX } from "react";
 
 export default function Overview(): JSX.Element {
 	return (
 		<div>
-			{" "}
-			<h1></h1>
-			<div>
-				<h1>Indicateurs clés</h1>
-				<div className="card">
-					<div className="cardHeader">
-						<img src="icon/admin/flame.svg" alt="" />
-						<h2>Total des inscriptions </h2>
-						<p>
-							Comptes inscrit depuis la création de la plateforme{" "}
-						</p>
-					</div>
-					<div className="cardBody">
-						<p>
-							{" "}
-							<img
-								className="icon"
-								src="icon/admin/tot.svg"
-								alt=""
-							/>
-							30
-						</p>
-					</div>
+			<h1>Indicateurs clés</h1>
+
+			<div className="card">
+				<div className="cardHeader">
+					<img src="icon/admin/flame.svg" alt="" />
+					<h2>Total des inscriptions</h2>
+					<p>Comptes inscrits depuis la création de la plateforme</p>
 				</div>
-				<div className="card">
-					<div className="cardHeader">
-						<img src="icon/admin/flame.svg" alt="" />
-						<h2>Taux de présence </h2>
-						<p>% de participants réellement présents</p>
-					</div>
-					<div className="cardBody">
-						<p>
-							{" "}
-							<img
-								className="icon"
-								src="icon/admin/tot.svg"
-								alt=""
-							/>
-							86%
-						</p>
-					</div>
-				</div>
-				<div className="card">
-					<div className="cardHeader">
-						<img src="icon/admin/flame.svg" alt="" />
-						<h2>Chiffre d’affaires</h2>
-						<p>
-							Comptes inscrit depuis la création de la plateforme
-						</p>
-					</div>
-					<div className="cardBody">
-						<p>
-							{" "}
-							<img
-								className="icon"
-								src="icon/admin/tot.svg"
-								alt=""
-							/>
-							30 000 / Année
-						</p>
-					</div>
+				<div className="cardBody">
+					<p>
+						<img className="icon" src="icon/admin/tot.svg" alt="" />
+						30
+					</p>
 				</div>
 			</div>
-			<div></div>
-			<div></div>
+
+			<div className="card">
+				<div className="cardHeader">
+					<img src="icon/admin/flame.svg" alt="" />
+					<h2>Taux de présence</h2>
+					<p>% de participants réellement présents</p>
+				</div>
+				<div className="cardBody">
+					<p>
+						<img className="icon" src="icon/admin/tot.svg" alt="" />
+						86%
+					</p>
+				</div>
+			</div>
+
+			<div className="card">
+				<div className="cardHeader">
+					<img src="icon/admin/flame.svg" alt="" />
+					<h2>Chiffre d’affaires</h2>
+					<p>Depuis la création de la plateforme</p>
+				</div>
+				<div className="cardBody">
+					<p>
+						<img className="icon" src="icon/admin/tot.svg" alt="" />
+						30 000 / an
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/pages/Admin/admin.tsx
+++ src/pages/Admin/admin.tsx
@@ -1,55 +1,32 @@
-// import { Outlet } from "react-router-dom";
 import "./admin.scss";
 import { type JSX } from "react";
-// import { Link } from "react-router-dom";
-// import { useLocation } from "react-router-dom";
-import Overview from "./overview/overview";
+import { Outlet } from "react-router-dom";
+
 export default function Admin(): JSX.Element {
 	return (
 		<section className="admin">
 			<aside>
 				<div className="titre">
 					<div className="box">
-						<h1> Swipe.</h1>
+						<h1>Swipe.</h1>
 						<p>Administrator</p>
 					</div>
-
 					<img src="icon/admin/switch.svg" alt="" />
 				</div>
 
 				<ul className="menu">
-					<li>
-						<img src="icon/admin/overview.svg" alt="" />
-						Vue d'ensemble
-					</li>
-					<li>
-						<img src="icon/admin/user.svg" alt="" />
-						Utilisateurs
-					</li>
-					<li>
-						<img src="icon/admin/stats.svg" alt="" />
-						Statistique
-					</li>
-					<li>
-						<img src="icon/admin/mail.svg" alt="" />
-						Notification
-					</li>
-					<li>
-						<img src="icon/admin/roue.svg" alt="" />
-						Paramètres
-					</li>
-					<li>
-						<img src="icon/admin/formation.svg" alt="" />
-						Formation
-					</li>
-					<li>
-						<img src="icon/admin/reservation.svg" alt="" />
-						Réservations
-					</li>
+					<li>Vue d'ensemble</li>
+					<li>Utilisateurs</li>
+					<li>Statistique</li>
+					<li>Notification</li>
+					<li>Paramètres</li>
+					<li>Formation</li>
+					<li>Réservations</li>
 				</ul>
 			</aside>
+
 			<div className="content">
-				<Overview></Overview>
+				<Outlet />
 			</div>
 		</section>
 	);
