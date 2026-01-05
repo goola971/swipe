336aa1b9ef27d6e0e3351ac5c23904f651e14d1d
emmanuel-kouassi <115140578+emmanuel-kouassi@users.noreply.github.com>
2026-01-05 23:44:41 +0100

    hb

--- src/pages/Admin/FormationAdmin/FormationAdmin.tsx
+++ src/pages/Admin/FormationAdmin/FormationAdmin.tsx
@@ -1,34 +1,34 @@
-import "./formationAdmin.scss";
-import { type JSX } from "react";
-import addFormation from "../../admin/addFormation/addFormation";
+// import "./formationAdmin.scss";
+// import { type JSX } from "react";
+// import addFormation from "../../admin/addFormation/addFormation";
 
-export default function FormationAdmin(): JSX.Element {
-	return (
-		<div className="formationAdmin">
-			<div className="headerformation">
-				<h2>Formations</h2>
-				<button>Ajouter une formation</button>
-			</div>
+// export default function FormationAdmin(): JSX.Element {
+// 	return (
+// 		<div className="formationAdmin">
+// 			<div className="headerformation">
+// 				<h2>Formations</h2>
+// 				<button>Ajouter une formation</button>
+// 			</div>
 
-			{/* <div className="formationcard">
-				<img src="img/cours/cours.png" alt="" />
-				<h2>Cours de Cybersécurité — Niveau Débutant</h2>
-				<p>
-					Découvrez les bases de la sécurité informatique : attaques
-					courantes, bonnes pratiques, premiers réflexes de défense.
-					Un cours clair, accessible et orienté pratique.
-				</p>
-				<div>
-					<button className="delete">Supprimer</button>
-					<button className="edit">Modifier</button>
-				</div>
-			</div> */}
-			<addFormation
-				titre="Ajouter une formation"
-				soustitre="primary"
-				delete=""
-				edit=""
-			/>
-		</div>
-	);
-}
+// 			{/* <div className="formationcard">
+// 				<img src="img/cours/cours.png" alt="" />
+// 				<h2>Cours de Cybersécurité — Niveau Débutant</h2>
+// 				<p>
+// 					Découvrez les bases de la sécurité informatique : attaques
+// 					courantes, bonnes pratiques, premiers réflexes de défense.
+// 					Un cours clair, accessible et orienté pratique.
+// 				</p>
+// 				<div>
+// 					<button className="delete">Supprimer</button>
+// 					<button className="edit">Modifier</button>
+// 				</div>
+// 			</div> */}
+// 			<addFormation
+// 				titre="Ajouter une formation"
+// 				soustitre="primary"
+// 				delete=""
+// 				edit=""
+// 			/>
+// 		</div>
+// 	);
+// }
