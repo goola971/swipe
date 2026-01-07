356d7fe9887dd3ef15374b7516bd773a4c9b4c25
emmanuel-kouassi <115140578+emmanuel-kouassi@users.noreply.github.com>
2026-01-07 15:21:59 +0100

    ajout de deconnexion

--- src/pages/Admin/admin.tsx
+++ src/pages/Admin/admin.tsx
@@ -1,13 +1,29 @@
 import "./admin.scss";
 import { type JSX, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 import FormationAdmin from "./FormationAdmin/FormationAdmin";
 import Overview from "./Overview/overview";
 import Users from "./userAdmin/userAdmin";
 import ReservationAdmin from "./reservationAdmin/reservationAdmin";
+
 export default function Admin(): JSX.Element {
     const [location, setLocation] = useState("overview");
+    const navigate = useNavigate();
+    const user = sessionStorage.getItem("user");
+    if (!user) {
+        navigate("/connexion", { replace: true });
+        return <></>; 
+    }
+
+// Fonction de déconnexion
+    const handleLogout = () => {
+        sessionStorage.clear(); 
+        localStorage.clear();
+        navigate("/connexion", { replace: true });
+        window.location.reload(); 
+    };
+
     return (
         <section className="admin">
             <aside>
@@ -66,6 +82,22 @@ export default function Admin(): JSX.Element {
                             Réservations
                         </Link>
                     </li>
+
+<li className="logout-item">
+    <a 
+        href="#" 
+        onClick={(e) => {
+            e.preventDefault(); // Empêche le comportement par défaut du lien
+            handleLogout();
+        }} 
+        className="logout-link"
+        style={{ cursor: 'pointer', display: 'flex', alignItems: 'center', gap: '10px' }}
+    >
+        <img src="icon/admin/logout.svg" alt="Logout" /> 
+        Déconnexion
+    </a>
+</li>
+
                 </ul>
             </aside>
             <div className="content">
